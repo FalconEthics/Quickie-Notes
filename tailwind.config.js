3c3d5fe1abd270d44f9677dd3a83dc611fe43323
Soumik Das <79033574+FalconEthics@users.noreply.github.com>
2022-12-06 14:56:19 +0530

    Revert "tailwind vercel bug fix"
    This reverts commit 03e2a7fd32674cd508aaaf525712de76b4b93e42.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: ["./src/**/*.{js,jsx,ts,tsx,html}",],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
